e2dcc0c47253d0f870aabd357175da4043df8ba6
Guillem <guillemguigo@gmail.com>
2021-02-22 12:09:13 +0100

    savepoint

--- scrap.py
+++ scrap.py
@@ -2,23 +2,27 @@ import requests
 from bs4 import BeautifulSoup
 import re
 import pandas as pd
-from utils import read_json_file, save_content_to_file
+from utils import read_json_file, save_content_to_file, makedir
 
-def get_soup_text(link, parser = 'lxml'):
-    """ CAT: retorna un 'soup element' a partir d'un localitzador uniforme de recursos (sigles URL en anglès)
-        EN: returns a 'soup element' from a URL
+def get_soup_html(link, parser = 'lxml'):
+    """ CAT: retorna un 'BeautifulSoup element' a partir d'un localitzador uniforme de recursos (sigles URL en anglès)
+        EN: returns a BeautifulSoup element from a URL
         
         arguments: URL (str), parser (str)
-        returns: soup element 
+        returns: BeautifulSoup element (bs4.BeautifulSoup)
     """
+    
     return BeautifulSoup(requests.get(link).text, parser)
 
 
 def get_names(bs_element, replacements = []):
-    """ CAT: retorna els noms
-        EN: 
-        returns: list of strings
+    """ CAT: transforma un BeautifulSoup Result Set a una llista de strings
+        EN: transforms a BeatifulSoup Result Set to a list of strings
+        
+        arguments: BeautifulSoup element (bs4.element.ResultSet), characters to replace (if any) (list of str)
+        returns: mushroom names (list of str)
     """
+    
     names = [name.text for name in bs_element]
     for char in replacements:
         names = [name.replace(char,'') for name in names]
@@ -26,18 +30,32 @@ def get_names(bs_element, replacements = []):
 
 
 def find_items(soup, tag_id, tag_especific, case='m'):
-    """ CAT: troba els elements específics
+    """ CAT: troba el contingut d'etiquetes' específiques d'un 'BeautifulSoup element'
+        EN: finds the content of specific tags of a 'BeautifulSoup element'
+        
+        arguments: BeautifulSoup element (bs4.BeautifulSoup), main tag (str), specific tag 
+                   (str, uppercase or lowercase search: 'm' or 'M'. Default: 'm', lowercase)
+        returns: Set of contents from the target tags (bs4.element.ResultSet)
     """
+    
     if case == 'M':
         return soup.find(*tag_id).findAll(*tag_especific)
     return soup.find(*tag_id).find_all(*tag_especific)
 
+
 def get_latin_names(names):
+    """ CAT: Troba els noms de les espècies en llatí a partir de la llista de noms en català
+        EN: Finds the latin name of the species from a list of names in catalan
+        
+        arguments: List of names in catalan (list of str)
+        returns: List of names in latin (list of str)
+    """
     
     names_latin = []
     for bolet in names:
+        # Afegim 'bolet' al final del nostre url de cerca
         link = 'https://www.google.com/search?q=' + bolet
-        soup = get_soup_text(link)
+        soup = BeautifulSoup(requests.get(link).text, parser)
         try:
             [find_name] = soup.find_all('a', href=True, text=re.compile("Wikipedia"))
             latin_name = re.findall(r'/wiki/(.*?)&', str(find_name))[0].replace('_',' ')
@@ -47,7 +65,7 @@ def get_latin_names(names):
             [new_name] = re.findall('wikipedia.org/wiki/(.*?)&amp', new_item)
             try:
                 new_link = "https://ca.wikipedia.org/wiki/" + new_name
-                new_soup = get_soup_text(new_link)
+                new_soup = BeautifulSoup(requests.get(new_link).text, parser)
                 latin_name = new_soup.find("div", {"class": 'mw-parser-output'}).find('i').text
             except AttributeError:
                 latin_name = new_name.replace("_"," ")
@@ -56,13 +74,16 @@ def get_latin_names(names):
         names_latin.append(latin_name)
     return names_latin
 
+
 def main():
+    
+    #carreguem les url on 
     df = pd.read_pickle('Noms/url comestibles')
 
     bolets_comestibles = []
 
     for idx in df.index:
-        soup = get_soup_text(df['url'][idx])
+        soup = BeautifulSoup(requests.get(df['url'][idx]).text, parser)
         text = find_items(soup, df['tag id'][idx], df['tag especific'][idx], case=df['grafia'][idx])
         names = get_names(text, replacements=df['replace'][idx])
         bolets_comestibles += names
@@ -93,4 +114,5 @@ def main():
     save_content_to_file(bolets,"Noms/bolets comestibles.txt","w")
 
 if __name__ == '__main__':
+    makedir("Noms/")
     main()
